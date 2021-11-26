67a0f7d83081d598a137e77e15b447609d9e5647
Saizuo <71127774+Saizuo@users.noreply.github.com>
2021-11-27 01:23:31 +0530

    S
    Z

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-var dropdown = document.getElementById("openMenu");
-var menuShow = document.getElementById("navbarMenuHeroC");
-
-dropdown.addEventListener("click", function () {
-  menuShow.classList.toggle("is-active");
-});
